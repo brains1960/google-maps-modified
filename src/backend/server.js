c364634b46ffbc08ff84e55d69d3390800b70298
brains1960 <tundunoladipo@gmail.com>
2018-09-08 06:03:17 -0400

    Setting up Routes

--- src/backend/server.js
+++ src/backend/server.js
@@ -3,3 +3,30 @@ import express from 'express';
 import passport from 'passport';
 import LocalStrategy from 'passport-local';
 import bodyParser from 'body-parser';
+
+const app = express();
+const routes = require('./routes/routes');
+// const auth = require('./routes/auth');
+
+// Initialize Passport
+
+app.use(express.static(path.join(__dirname, 'build')));
+app.use(bodyParser.json());
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use('/', auth(passport));
+app.use('/', routes);
+
+// Initialize Mongoose
+mongoose.connection.on('connected', () => {
+  console.log('connected to mongoDB');
+});
+mongoose.connect(process.env.MONGODB_URI);
+
+module.exports = app;
+
+server.listen(1337, '127.0.0.1');
+
+console.log('Server running at http://127.0.0.1:1337/')
